0373ed6ff26a47422e7a4647c787fd2b6eee65bb
Gilles Mottiez <gilles.mottiez@students.hevs.ch>
2019-01-17 16:04:50 +0100

    code

--- Prog/C5505_CCS8/iir_filters_for_students/Debug/fix_point_iir_example_for_students.d
+++ Prog/C5505_CCS8/iir_filters_for_students/Debug/fix_point_iir_example_for_students.d
@@ -1,17 +1,17 @@
 # FIXED
 
 fix_point_iir_example_for_students.obj: ../fix_point_iir_example_for_students.c
-fix_point_iir_example_for_students.obj: C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/stdio.h
-fix_point_iir_example_for_students.obj: C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/linkage.h
-fix_point_iir_example_for_students.obj: C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/stdarg.h
-fix_point_iir_example_for_students.obj: C:/Temp/Test/C5505_CCS8/include/usbstk5505.h
-fix_point_iir_example_for_students.obj: C:/Temp/Test/C5505_CCS8/include/usbstk5505_led.h
+fix_point_iir_example_for_students.obj: C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/stdio.h
+fix_point_iir_example_for_students.obj: C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/linkage.h
+fix_point_iir_example_for_students.obj: C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/stdarg.h
+fix_point_iir_example_for_students.obj: C:/Users/gilles.mottiez/Desktop/C5505_CCS8/include/usbstk5505.h
+fix_point_iir_example_for_students.obj: C:/Users/gilles.mottiez/Desktop/C5505_CCS8/include/usbstk5505_led.h
 fix_point_iir_example_for_students.obj: ../aic3204_init.h
 
 ../fix_point_iir_example_for_students.c: 
-C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/stdio.h: 
-C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/linkage.h: 
-C:/ti/ccsv8/tools/compiler/c5500_4.4.1/include/stdarg.h: 
-C:/Temp/Test/C5505_CCS8/include/usbstk5505.h: 
-C:/Temp/Test/C5505_CCS8/include/usbstk5505_led.h: 
+C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/stdio.h: 
+C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/linkage.h: 
+C:/ti/C5500\ Code\ Generation\ Tools\ 4.4.1/include/stdarg.h: 
+C:/Users/gilles.mottiez/Desktop/C5505_CCS8/include/usbstk5505.h: 
+C:/Users/gilles.mottiez/Desktop/C5505_CCS8/include/usbstk5505_led.h: 
 ../aic3204_init.h: 
---
+++ Prog/C5505_CCS8/lib/Debug/bsl/subdir_vars.mk
@@ -0,0 +1,44 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../bsl/usbstk5505.c \
+../bsl/usbstk5505_gpio.c \
+../bsl/usbstk5505_i2c.c \
+../bsl/usbstk5505_led.c 
+
+C_DEPS += \
+./bsl/usbstk5505.d \
+./bsl/usbstk5505_gpio.d \
+./bsl/usbstk5505_i2c.d \
+./bsl/usbstk5505_led.d 
+
+OBJS += \
+./bsl/usbstk5505.obj \
+./bsl/usbstk5505_gpio.obj \
+./bsl/usbstk5505_i2c.obj \
+./bsl/usbstk5505_led.obj 
+
+OBJS__QUOTED += \
+"bsl\usbstk5505.obj" \
+"bsl\usbstk5505_gpio.obj" \
+"bsl\usbstk5505_i2c.obj" \
+"bsl\usbstk5505_led.obj" 
+
+C_DEPS__QUOTED += \
+"bsl\usbstk5505.d" \
+"bsl\usbstk5505_gpio.d" \
+"bsl\usbstk5505_i2c.d" \
+"bsl\usbstk5505_led.d" 
+
+C_SRCS__QUOTED += \
+"../bsl/usbstk5505.c" \
+"../bsl/usbstk5505_gpio.c" \
+"../bsl/usbstk5505_i2c.c" \
+"../bsl/usbstk5505_led.c" 
+
+
